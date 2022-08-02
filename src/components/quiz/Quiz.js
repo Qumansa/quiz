f79f109b351c6b1df4c6cde40f5a24771fdc0494
Qumansa <qumansa@yandex.ru>
2022-08-02 14:29:58 +0400

    Квиз
    Вместо количества правильных ответов показывается текущий уровень сложности

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -20,16 +20,16 @@ import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Result from '../result/Result';
 
-import transformCurrentDifficulty from '../utils/transformCurrentDifficulty';
-import reload from '../utils/reload';
+import transformCurrentDifficulty from '../../utils/transformCurrentDifficulty';
+import reload from '../../utils/reload';
 
 import './quiz.sass';
 
 const Quiz = () => {
     const {data: isQuizOver = false} = useGetIsQuizOverQuery();
 
-    let {data: currentDifficulty} = useGetCurrentDifficultyQuery();
-    currentDifficulty = transformCurrentDifficulty(currentDifficulty);
+    const {data: currentDifficultyNotTransformed = '-'} = useGetCurrentDifficultyQuery();
+    const currentDifficulty = transformCurrentDifficulty(currentDifficultyNotTransformed);
 
     const {data: questions = []} = useGetQuestionsQuery(currentDifficulty);
     const totalAmountOfQuestions = questions.length;
@@ -44,7 +44,7 @@ const Quiz = () => {
         isError,
     } = useGetQuestionQuery(`${currentDifficulty}/${indexOfCurrentQuestion}`);
 
-    const {id = '-', description, answers, correctAnswer, skipped} = isFetching || isError
+    const {id = '-', description, answers = ['-', '-', '-', '-'], correctAnswer, skipped} = isFetching || isError
         ? {}
         : questionData;
 
@@ -149,30 +149,28 @@ const Quiz = () => {
             : <p className="question__descr">{description}</p>;
 
         const getAnswersView = (arr = ['-', '-', '-', '-']) => {
-            return arr.map((answer, index) => {
-                return (
-                    <li 
-                        className="answers__item"
-                        key={index}>
-                        <input 
-                            id={`answer${index}`}
-                            className="answers__radio sr-only"
-                            type="radio"
-                            name="answer"
-                            value={answer}
-                            required/>
-                        <label 
-                            className="answers__descr" 
-                            htmlFor={`answer${index}`}>
-                            {isFetching 
-                                ? "Загрузка..." 
-                                : isError
-                                ? <ErrorMessage/>
-                                : answer}
-                        </label>
-                    </li>
-                );
-            });
+            return arr.map((answer, index) =>  (
+                <li 
+                    className="answers__item"
+                    key={index}>
+                    <input 
+                        id={`answer${index}`}
+                        className="answers__radio sr-only"
+                        type="radio"
+                        name="answer"
+                        value={answer}
+                        required/>
+                    <label 
+                        className="answers__descr" 
+                        htmlFor={`answer${index}`}>
+                        {isFetching 
+                            ? "Загрузка..." 
+                            : isError
+                            ? <ErrorMessage/>
+                            : answer}
+                    </label>
+                </li>
+            ));
         };
 
         const answersView = getAnswersView(answers);
@@ -211,14 +209,16 @@ const Quiz = () => {
             <section className="section section_first">
                 <div className="container">
                     <h1 className="sr-only">Викторина</h1>
-                    <div className="question__counter">
-                        <svg className="question__icon" viewBox="-1 0 18 15" fill="white" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M15.4829 5.28193L10.6864 4.59758L8.54222 0.330127C8.48365 0.213287 8.38731 0.118702 8.26829 0.0612089C7.96981 -0.0834504 7.6071 0.037099 7.45786 0.330127L5.3137 4.59758L0.517213 5.28193C0.384974 5.30047 0.26407 5.36167 0.171503 5.45441C0.0595945 5.56733 -0.0020722 5.71924 5.31712e-05 5.87678C0.00217854 6.03431 0.0679221 6.18457 0.182838 6.29454L3.65316 9.61614L2.83328 14.3064C2.81405 14.4156 2.82635 14.5278 2.86878 14.6304C2.91121 14.733 2.98207 14.8218 3.07333 14.8869C3.16459 14.952 3.27259 14.9906 3.38509 14.9985C3.4976 15.0064 3.6101 14.9831 3.70983 14.9314L8.00004 12.717L12.2902 14.9314C12.4074 14.9926 12.5434 15.013 12.6737 14.9908C13.0024 14.9352 13.2235 14.6291 13.1668 14.3064L12.3469 9.61614L15.8172 6.29454C15.9117 6.20367 15.974 6.08497 15.9929 5.95515C16.0439 5.63059 15.8135 5.33015 15.4829 5.28193Z" fill="white"></path>
-                        </svg>
-                        <span className="question__text">
-                            Вопрос <span className="question__number">{id}</span>/{totalAmountOfQuestions}
-                        </span>
-                        Количество правильных ответов: {amountOfCorrectAnswers}
+                    <div className="question__header">
+                        <div className="question__counter">
+                            <svg className="question__icon" viewBox="-1 0 18 15" fill="white" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M15.4829 5.28193L10.6864 4.59758L8.54222 0.330127C8.48365 0.213287 8.38731 0.118702 8.26829 0.0612089C7.96981 -0.0834504 7.6071 0.037099 7.45786 0.330127L5.3137 4.59758L0.517213 5.28193C0.384974 5.30047 0.26407 5.36167 0.171503 5.45441C0.0595945 5.56733 -0.0020722 5.71924 5.31712e-05 5.87678C0.00217854 6.03431 0.0679221 6.18457 0.182838 6.29454L3.65316 9.61614L2.83328 14.3064C2.81405 14.4156 2.82635 14.5278 2.86878 14.6304C2.91121 14.733 2.98207 14.8218 3.07333 14.8869C3.16459 14.952 3.27259 14.9906 3.38509 14.9985C3.4976 15.0064 3.6101 14.9831 3.70983 14.9314L8.00004 12.717L12.2902 14.9314C12.4074 14.9926 12.5434 15.013 12.6737 14.9908C13.0024 14.9352 13.2235 14.6291 13.1668 14.3064L12.3469 9.61614L15.8172 6.29454C15.9117 6.20367 15.974 6.08497 15.9929 5.95515C16.0439 5.63059 15.8135 5.33015 15.4829 5.28193Z" fill="white"></path>
+                            </svg>
+                            <span className="question__text">
+                                Вопрос <span className="question__number">{id}</span>/{totalAmountOfQuestions}
+                            </span>
+                        </div>
+                        <span className="question__dofficulty">Уровень сложности: {currentDifficultyNotTransformed}</span>
                     </div>
                     <div className="question__wrapper">
                         {descriptionView}
--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -119,34 +119,32 @@ const Start = () => {
             : null;
 
         return (
-            <>
-                <section className="start section">
-                    <div className="container">
-                        <h1 className="title">Quiz</h1>
-                        <p className="start__descr">Добро пожаловать в викторину "Quiz"!</p>
-                        <p className="start__descr">Здесь Вы сможете попробовать свои силы и проверить знания, отвечая на различные вопросы о веб-разработке.</p>
-                        <p className="start__descr">Существует 3 уровня сложности: легкий, средний и сложный. В каждом уровне сложности содержится по 10 вопросов. Для того, чтобы пройти уровень, необходимо успешно ответить на 7/10 вопросов, после чего откроется следующий. Если Вы затрудняетесь ответить, вопрос можно пропустить и вернуться к нему позднее.</p>
-                        <div className="start__difficulty">
-                            Выберите уровень сложности:
-                            {difficultiesView}
-                        </div>
-                        <p className="start__descr">Желаем удачи!</p>
-                        <ul className="start__links">
-                            <li className="start__links-item">
-                                <Link 
-                                    className="
-                                        start__links-link 
-                                        button" 
-                                    to="/quiz"
-                                    onClick={onStart}>
-                                    Начать
-                                </Link>
-                            </li>
-                            {continueLinkView}
-                        </ul>
+            <section className="start section">
+                <div className="container">
+                    <h1 className="title">Quiz</h1>
+                    <p className="start__descr">Добро пожаловать в викторину "Quiz"!</p>
+                    <p className="start__descr">Здесь Вы сможете попробовать свои силы и проверить знания, отвечая на различные вопросы о веб-разработке.</p>
+                    <p className="start__descr">Существует 3 уровня сложности: легкий, средний и сложный. В каждом уровне сложности содержится по 10 вопросов. Для того, чтобы пройти уровень, необходимо успешно ответить на 7/10 вопросов, после чего откроется следующий. Если Вы затрудняетесь ответить, вопрос можно пропустить и вернуться к нему позднее.</p>
+                    <div className="start__difficulty">
+                        Выберите уровень сложности:
+                        {difficultiesView}
                     </div>
-                </section>
-            </>
+                    <p className="start__descr">Желаем удачи!</p>
+                    <ul className="start__links">
+                        <li className="start__links-item">
+                            <Link 
+                                className="
+                                    start__links-link 
+                                    button" 
+                                to="/quiz"
+                                onClick={onStart}>
+                                Начать
+                            </Link>
+                        </li>
+                        {continueLinkView}
+                    </ul>
+                </div>
+            </section>
         );
     };
 
